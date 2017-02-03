777c302ad920cde81f1aa1da39e5282ea35baa74
dbairaktaris1 <Dbairaktaris1@luce.edu>
2017-02-02 23:23:38 -0600

    Added basic Facility/Building/Room methods and attributes...Some may still need some work in the method definitions.

--- src/edu/luc/cs/comp473/project1/model/facility/Facility.java
+++ src/edu/luc/cs/comp473/project1/model/facility/Facility.java
@@ -1,5 +1,43 @@
+/**
+ * 
+ */
 package edu.luc.cs.comp473.project1.model.facility;
+import java.util.* ; 
 
 public class Facility {
-
+	
+	private String name ;
+	private String description; 
+	private List<Building> buildings ; 
+	
+	public Facility(String name, String description){
+		this.name = name ; 
+		this.description = description ; 
+	}
+	
+	public String getName(){
+		return this.name ; 
+	}
+	
+	public String getDescription(){
+		return description ; 
+	}
+	
+	public List<Building> getBuildings(){
+		return buildings ; 
+	}
+	
+	public String getFacilityInformation(){
+		//TODO
+		return "" ; 
+	}
+	
+	public void addFacilityDetail(){
+		//TODO : Figure out parameter, figure out what "detail" means	
+	}
+	
+	public int requestAvailableCapacity(){
+		int capacity = 0 ;
+		return 0 ; 
+	}
 }
--- src/edu/luc/cs/comp473/project1/model/facility/Room.java
+++ src/edu/luc/cs/comp473/project1/model/facility/Room.java
@@ -2,4 +2,21 @@ package edu.luc.cs.comp473.project1.model.facility;
 
 public class Room {
 
+	private int roomNumber ; 
+	private int capacity ; 
+	private boolean available ;
+	
+	public Room(int roomNumber, int capacity){
+		this.roomNumber = roomNumber ;
+		this.capacity = capacity ;
+		this.available = true ;
+	}
+	
+	public void setAvailability(boolean v){
+		this.available = v ;
+	}
+	
+	public boolean isAvailable(){
+		return this.available;
+	}
 }
