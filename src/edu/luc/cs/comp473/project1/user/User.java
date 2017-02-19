ac40be92ab49365a95a9ce8443b3cd1ef045ed61
Thaddeus Krawczyk <tkrawczyk@luc.edu>
2017-02-19 12:34:21 -0600

    started creating user driver

--- src/edu/luc/cs/comp473/project1/user/User.java
+++ src/edu/luc/cs/comp473/project1/user/User.java
@@ -1,10 +1,19 @@
 package edu.luc.cs.comp473.project1.user;
 
+import edu.luc.cs.fms.model.facility.ConcreteFacilityManager;
+import edu.luc.cs.fms.model.facility.FacilityManager;
+
 public class User {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+        FacilityManager manager = new ConcreteFacilityManager();
+        
+        manager.addNewFacility("WTC", "Loyola University Chicago Water Tower Campus", "111 E. Pearson St.");
+        manager.addNewFacility("LSC", "Loyola University Chicago Lake Shore Campus", "1032 W. Sheridan Rd.");
+        
+        
+        
     }
 
 }
