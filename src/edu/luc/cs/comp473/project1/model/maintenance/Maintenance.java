883c9b351f331496d55f3b49ce7885bbf2e169e0
dbairaktaris1 <dbairaktaris1@luc.edu>
2017-02-19 01:41:26 -0600

    Removed comments

--- src/edu/luc/cs/comp473/project1/model/maintenance/Maintenance.java
+++ src/edu/luc/cs/comp473/project1/model/maintenance/Maintenance.java
@@ -6,14 +6,14 @@ import java.util.List;
 
 public interface Maintenance {
 
-    public void makeFacilityMaintRequest(String problem);//
+    public void makeFacilityMaintRequest(String problem);
     public boolean scheduleMaintenance(Date date1, Date date2);
     public BigDecimal calcMaintenanceCostForFacility();
     public float calcProblemRateForFacility();
     public long calcDownTimeForFacility();
-    public List<MaintenanceRequest> listMaintRequests();//
-    public List<Order> listMaintenance();//
-    public String listFacilityProblems();//
+    public List<MaintenanceRequest> listMaintRequests();
+    public List<Order> listMaintenance();
+    public String listFacilityProblems();
     public void createOrder(String desc, int orderNum);
     public void setLaborCost(BigDecimal cost, int orderNum);
     public void setPartsCost(BigDecimal cost, int orderNum);
